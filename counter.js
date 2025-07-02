61b50bdd2f86a93056181518b7413e03cea2dfd8
Chris Ruiz <25648673+csr-7@users.noreply.github.com>
2025-07-02 11:07:37 -0700

    updated api url

--- counter.js
+++ counter.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://u2ekr4m9dl.execute-api.us-west-1.amazonaws.com/crc';
+const API_URL = 'https://u2ekr4m9dl.execute-api.us-west-1.amazonaws.com/crc/visitor-count';
 
         async function updateVisitorCount() {
             try {
