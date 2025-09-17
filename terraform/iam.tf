7c0ff411ce1331ad7cab9c46fda883c87924ac81
Chris Admin Ruiz <cruiz-admin@FAIRMONTSCHOOLS.COM>
2025-09-17 16:02:22 -0700

    iam.tf edits

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -26,7 +26,7 @@ resource "aws_iam_role" "github_actions_role" {
       {
         Effect = "Allow",
         Principal = {
-          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+          Federated = "arn:aws:iam::205930604749:oidc-provider/token.actions.githubusercontent.com"
         },
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
